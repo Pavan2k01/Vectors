ff05ee71c277f1115f1d9682c33e869301ab7098
Pavan2k01 <133356414+Pavan2k01@users.noreply.github.com>
2023-07-18 13:59:17 +0530

    Update vector.py

--- Vectors/Code/vector.py
+++ Vectors/Code/vector.py
@@ -1,33 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import symbols, Eq, solve
+import math
+
 #line 1
 def line1(x):
   return (2-3*x)
-#line 1
+#line 2
 def line2(x):
    return (-3+2*x)
-
-x, y = symbols('x y')
-
-# Define the equations
-eq1 = Eq(3*x + y - 2, 0)
-eq2 = Eq(2*x - y - 3, 0)
-
-# Solve the system of equations
-solution = solve((eq1, eq2), (x, y))
-
-# Extract the solutions for x and y
-x_solution = solution[x]
-y_solution = solution[y]
-print("Solution:")
-print(f"x = {x_solution}")
-print(f"y = {y_solution}")
-
 def line3(x):
   return (3 - ((3 - 2*y_solution)/x_solution)*x)/2
+
+#Define the coefficient matrix
+A = np.array([[3, 1], [2, -1]])
+
+#Defne the Vector
+b = np.array([2, 3])
+
+#solve the equations
+solution = np.linalg.solve(A, b)
+
+#Extract the Values
+x_solution = solution[0]
+y_solution = solution[1]
+p_solution = (3-2*y_solution)/(x_solution)
+
+#print the values
+print(f"x : {math.trunc(x_solution)}")
+print(f"y : {math.trunc(y_solution)}")
+print(f"p : {math.trunc(p_solution)}")
+
 # Define the x values for the lines
-x_values = np.linspace(-3, 3, 30)
+x_values = np.linspace(-10, 10, 100)
 
 # Calculate the y values for each line
 y_values1 = line1(x_values)
@@ -54,10 +58,10 @@ plt.axvline(0, color='black', linewidth=0.5)
 def display_coordinates(x, y):
     plt.scatter(x, y, color='black')  # Plot the point
     plt.text(x, y, f'({x}, {y})', verticalalignment='bottom')
-display_coordinates((x_solution + y_solution), 0)
-display_coordinates(0,(x_solution + y_solution))
-display_coordinates(x_solution,y_solution)
+#display_coordinates((x_solution + y_solution), 0)
+#display_coordinates(0,(x_solution + y_solution))
+display_coordinates(math.trunc(x_solution), math.trunc(y_solution))
 
-plt.xlim(-3, 3)
-plt.ylim(-3, 3)
+plt.xlim(-1, 3)
+plt.ylim(-3, 1)
 plt.legend()
